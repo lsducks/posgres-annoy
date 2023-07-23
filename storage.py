3025665327f1fcd1d9540e1c19dba83b27510c17
Sergey <s_levsha@bk.ru>
2023-07-23 22:39:20 +0300

    Updated __get_columns

--- storage.py
+++ storage.py
@@ -1,4 +1,4 @@
-from typing import Sequence, List, Union, Literal
+from typing import Sequence, List, Literal
 from functools import lru_cache
 
 import psycopg2
@@ -16,7 +16,8 @@ from nltk.corpus import stopwords
 
 class Storage:
     def __init__(
-        self, 
+        self,
+        table_names: list,
         host: str = 'localhost',
         port: int = 5432,
         dbname: str = 'postgres',
@@ -29,14 +30,41 @@ class Storage:
 
         self.cursor = connection.cursor()
 
-        self.columns = (
-            'title_id titletype primarytitle originaltitle ' +
-            'isadult startyear endyear runtimeminutes genres'
-        ).split()
+        # self.columns = (
+        #     'title_id titletype primarytitle originaltitle ' +
+        #     'isadult startyear endyear runtimeminutes genres'
+        # ).split()
+
+        self.columns = self.__get_columns(table_names)
 
         self.morph = pymorphy3.MorphAnalyzer()
         self.stopwords = set(stopwords.words("russian"))
-                
+
+
+    def __get_columns(
+        self,
+        table_names: list,
+    ) -> list:
+        query_template = jinja2.Template("""
+           SELECT column_name
+           FROM INFORMATION_SCHEMA.COLUMNS
+           WHERE TABLE_NAME = '{{ table_names[0] }}'
+           {% if table_names|length > 1 -%}
+           {% for table_name in table_names[1:] -%}
+           UNION
+           SELECT column_name
+           FROM INFORMATION_SCHEMA.COLUMNS
+           WHERE TABLE_NAME = '{{ table_name }}' 
+           {%- endfor %}
+           {%- endif %}
+        """)
+
+        query = query_template.render(table_names=table_names)
+
+        self.cursor.execute(query)
+
+        return self.cursor.fetchall()
+
     def query(
         self, 
         min_year: int | None = None,
@@ -45,12 +73,17 @@ class Storage:
         min_time: int | None = None,
         max_time: int | None = None,
         title_type: str | None = None,
+        min_rating: float | None = None,
+        n_votes: int | None = None,
         limit: int | None = None,
         genre: str | None = None
     ) -> pd.DataFrame:
         query_template = jinja2.Template("""
         select *
             from titles
+            {% if min_rating -%}
+            inner join ratings using(title_id)
+            {%- endif %}
             where endyear IS NULL
             {% if is_adult -%}
             and isadult = {{ is_adult }}
@@ -70,6 +103,12 @@ class Storage:
             {% if max_year -%}
             and startyear <= {{ max_year }}
             {%- endif %}
+            {% if min_rating -%}
+            and avg_rating >= {{ min_rating }}
+            {%- endif %}
+            {% if n_votes -%}
+            and n_votes >= {{ n_votes }}
+            {%- endif %}
             order by startyear desc
             {% if limit %}
             limit {{ limit }}
@@ -82,6 +121,8 @@ class Storage:
             max_time=max_time,
             min_year=min_year,
             max_year=max_year,
+            min_rating=min_rating,
+            n_votes=n_votes,
             limit=limit
         )
         
@@ -166,7 +207,11 @@ class Storage:
         self.annoy_storage.load(filepath_annoy)
 
 
-example = Storage()
-example.load('./attrs.joblib', './annoy.ann')
+example = Storage(['titles', 'ratings'])
 
-# print(example.query(title_type='movie', min_year=2000, is_adult=0, min_time=90, max_time=180, genre='Horror'))
+print(example.columns)
+
+example.load('./data/attrs.joblib', './data/annoy.ann')
+
+print(example.query(title_type='movie', min_year=2000, is_adult=0, min_time=90,
+                    max_time=180, min_rating=4, n_votes=1000, limit=50))
